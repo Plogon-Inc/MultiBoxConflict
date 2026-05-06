292452222b3d0e762e5c3493167593884e91b14e
Y <username@users.noreply.github.com>
2026-05-06 23:56:03 +0200

    keep playing with externals

--- MultiBoxConflict/Service/NoMoreConflictManager.cs
+++ MultiBoxConflict/Service/NoMoreConflictManager.cs
@@ -374,7 +374,7 @@ public unsafe class MultiBoxConflictManager : IDisposable
             var playerNames = Svc.Objects.PlayerObjects.Select(p => p.Name.TextValue);
             HasExternalPlayers = !Config.RegisteredCharacters.ContainsAll(playerNames);
             MatchStatus = (Config.Wintrade && !HasExternalPlayers && Config.RegisteredLosers.Contains(Svc.PlayerState.CharacterName)) ? "anti_afk" : "match_start";
-            if(MatchStatus == "anti_afk" && Config.TeamUp && Svc.Objects.PlayerObjects.Any(o => !o.IsDead && !o.IsHostile() && Config.RegisteredCharacters.Contains(o.Name.TextValue)))
+            if(Config.RegisteredCharacters.Count > 0 && MatchStatus == "anti_afk" && Config.TeamUp && Svc.Objects.PlayerObjects.Where(o => !o.IsDead && !o.IsHostile()).Select(o=>o.Name.TextValue).Contains(Config.RegisteredCharacters[0]))
                 MatchStatus = "match_start";
             
             Aggro.Reset();
